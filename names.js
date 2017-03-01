8b303515fb8591779cb105f3e15aa4a4ac61d6c7
Edvard A. M. Haaland <fishurban@gmail.com>
2017-03-01 08:26:36 +0100

    This file is dumb
    and useless
    
    because I'm dumb

--- names.js
+++
@@ -1,4 +0,0 @@
-var elfNames = {elfMale, elfFemale, elfSurname};
-var elfMale = ["Adran", "Aelar", "Aramil", "Arannis", "Aust"];
-var elfFemale = ["Adrie", "Althaea", "Anastrianna", "Andraste", "Antinua"];
-var elfSurname = ["Amakiir", "Amastacia", "Galanodel", "Holimion", "Ilphelkiir", "Liadon"];
